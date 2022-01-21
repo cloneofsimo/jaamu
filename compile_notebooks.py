ba7ba6909b53c6c451908365492b2233489e00ed
Simo Ryu <cloneofsimo@gmail.com>
2022-01-21 19:21:03 +0000

    made testing ipynbs

--- compile_notebooks.py
+++ compile_notebooks.py
@@ -4,12 +4,28 @@ import glob
 
 def compile_notebooks():
     # jupyter nbconvert --to html /home/simo/devs/blog/simoryu-blog/src/notebooks/Trigonometric-Harmonic-series::Simo_Ryu_::2019-05-04.ipynb --output-dir /home/simo/devs/blog/simoryu-blog/src/notebooks/compiled_htmls --template basic
-    #INPUT = "/home/simo/devs/blog/simoryu-blog/src/notebooks/Trigonometric-Harmonic-series::Simo_Ryu_::2019-05-04.ipynb"
+    # INPUT = "/home/simo/devs/blog/simoryu-blog/src/notebooks/Trigonometric-Harmonic-series::Simo_Ryu_::2019-05-04.ipynb"
     OUTPUT_DIR = "./src/notebooks/compiled_htmls"
 
     # activate conda
-    #subprocess.run([])
+    # subprocess.run([])
     for file in glob.glob("./src/notebooks/*.ipynb"):
-        subprocess.call(["jupyter", "nbconvert", "--to", "html", file, "--output-dir", OUTPUT_DIR, "--template", "basic", "--no-input"])
+        subprocess.call(
+            [
+                "jupyter",
+                "nbconvert",
+                "--RegexRemovePreprocessor.patterns",  # =[#rm]""",
+                "#rm.*",
+                "--to",
+                "html",
+                file,
+                "--output-dir",
+                OUTPUT_DIR,
+                "--template",
+                "basic",
+                "--no-prompt",
+            ]
+        )
+
 
 compile_notebooks()
