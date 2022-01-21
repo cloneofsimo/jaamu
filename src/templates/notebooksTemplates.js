9424b96bf876f2f6c60ce27e2664f202eb232e8c
Simo Ryu <cloneofsimo@gmail.com>
2022-01-21 20:18:40 +0000

    larger container

--- src/templates/notebooksTemplates.js
+++ src/templates/notebooksTemplates.js
@@ -22,34 +22,32 @@ const NotebookTemplate = (props) => {
   const { htmlArgs, htmlContent } = pageContext;
   const color = useColorModeValue("black", "gray.800");
 
-  const {author, tag, title, abstract} = htmlArgs;
+  const { author, tag, title, abstract } = htmlArgs;
 
   console.log(pageContext);
   return (
-    
-      <div>
-        <h1>Hi!</h1>
-        <Button
-          position="fixed"
-          right="1rem"
-          top="1rem"
-          onClick={toggleColorMode}
-        >
-          Toggle {colorMode === "light" ? "Dark" : "Light"}
-        </Button>
-        <Header />
-        <h1>Title: {title}</h1>
-        <h2>Author: {author}</h2>
-        <h3>Tags: {tag}</h3>
-        <p>Abstract: {abstract}</p>
-        
-        <Container>
-          <div dangerouslySetInnerHTML={{ __html: htmlContent }} />
-        </Container>
+    <div>
+      <h1>Hi!</h1>
+      <Button
+        position="fixed"
+        right="1rem"
+        top="1rem"
+        onClick={toggleColorMode}
+      >
+        Toggle {colorMode === "light" ? "Dark" : "Light"}
+      </Button>
+      <Header />
+      <h1>Title: {title}</h1>
+      <h2>Author: {author}</h2>
+      <h3>Tags: {tag}</h3>
+      <p>Abstract: {abstract}</p>
 
-        <Footer />
-      </div>
-    
+      <Container maxW="container.xl">
+        <div dangerouslySetInnerHTML={{ __html: htmlContent }} />
+      </Container>
+
+      <Footer />
+    </div>
   );
 };
 
