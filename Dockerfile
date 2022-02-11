6ac1aed24ffbd65f8ad1df7852f62308f9b1c383
ChungHwan Han <51526347+hanchchch@users.noreply.github.com>
2022-02-11 15:31:02 +0900

    fix: use symlink on lambda function (#3)
    * fix: optimize dockerfile
    
    * fix: use symlink on lambda function

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,18 @@
 FROM amazon/aws-lambda-python:3.8
 
 WORKDIR /var/task
-COPY . .
+COPY requirements.txt .
 RUN pip install -r requirements.txt
 
 RUN yum update -y
 RUN yum install -y gcc-c++ make curl
 RUN curl -sL https://rpm.nodesource.com/setup_14.x | bash -
 RUN yum install nodejs npm -y
+
+COPY *.json .
 RUN npm install
+
+COPY . .
 
 # lambda_function.handler 실행
 CMD ["lambda_function.handler"]
--- lambda_function.py
+++ lambda_function.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 import subprocess
 import boto3
 import mimetypes
@@ -39,27 +40,81 @@ def upload_bucket(bucket, source_dir):
 
             bucket.upload_file(
                 Filename=full_path,
-                Key=full_path[len(source_dir) + 1 :],
+                Key=full_path[len(source_dir) :],
                 ExtraArgs={"ContentType": mimetype},
             )
 
 
+def clear_bucket(bucket):
+    bucket = s3.Bucket(bucket)
+    bucket.objects.all().delete()
+
+
+def clear_directory(directory):
+    for filename in os.listdir(directory):
+        filepath = os.path.join(directory, filename)
+        try:
+            if os.path.isfile(filepath) or os.path.islink(filepath):
+                os.unlink(filepath)
+            elif os.path.isdir(filepath):
+                shutil.rmtree(filepath)
+        except Exception as e:
+            print("Failed to delete %s. Reason: %s" % (filepath, e))
+
+
+def create_symlink(source_dir, target_dir):
+    for filename in os.listdir(source_dir):
+        filepath = os.path.join(source_dir, filename)
+        print(f"Symlinking {filepath} to {target_dir}")
+
+        if (
+            NOTEBOOK_DIR.replace(target_dir, source_dir).startswith(filepath)
+            or STATIC_DIR.replace(target_dir, source_dir).startswith(filepath)
+            or PUBLIC_DIR.replace(target_dir, source_dir).startswith(filepath)
+        ):
+            print("passing")
+            continue
+
+        os.symlink(
+            filepath,
+            os.path.join(target_dir, filename),
+            os.path.isdir(filepath),
+        )
+
+
+def build_and_publish():
+    print("Clear mounted directory.")
+    clear_directory(mount_dir)
+
+    print("Copy notebook files from S3.")
+    download_bucket(os.environ.get("S3_CACHE_BUCKET"), NOTEBOOK_DIR)
+
+    print("Copy static files from S3.")
+    download_bucket(os.environ.get("S3_THUMBNAIL_BUCKET"), STATIC_DIR)
+
+    print("Symlinking the source codes.")
+    cwd = os.getcwd()
+    create_symlink(cwd, mount_dir)
+    create_symlink(os.path.join(cwd, "src"), os.path.join(mount_dir, "src"))
+
+    print("Build gatsby.")
+    subprocess.check_output(("npm", "--prefix", mount_dir, "run", "build"))
+
+    print("Clear the blog bucket.")
+    clear_bucket(os.environ.get("S3_BLOG_BUCKET"))
+
+    print("Copy output files to S3.")
+    upload_bucket(os.environ.get("S3_BLOG_BUCKET"), PUBLIC_DIR)
+
+    return os.listdir(PUBLIC_DIR)
+
+
 def handler(event, context):
     try:
-        print("Copy notebook files from S3.")
-        download_bucket(os.environ.get("S3_CACHE_BUCKET"), NOTEBOOK_DIR)
-
-        print("Copy static files from S3.")
-        download_bucket(os.environ.get("S3_THUMBNAIL_BUCKET"), STATIC_DIR)
-
-        print("Build gatsby.")
-        args = ("npm", "--prefix", mount_dir, "run", "build")
-        subprocess.check_output(args)
-
-        print("Copy output files to S3.")
-        upload_bucket(os.environ.get("S3_BLOG_BUCKET"), PUBLIC_DIR)
-
-        return {"status_code": 200, "message": "Success"}
-
+        published = build_and_publish()
+        return {
+            "status_code": 200,
+            "message": str(published),
+        }
     except Exception as e:
         return {"status_code": 500, "message": str(e)}
